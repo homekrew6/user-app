a484f51cd0975be302cb3f2bf4bbcfc58f587a38
Ravi S. Singh <nits.ravis@gmail.com>
2018-02-08 17:47:47 +0530

    app menu

--- js/components/accounts/styles.js
+++ js/components/accounts/styles.js
@@ -33,20 +33,96 @@ export default {
     bottom: 6,
     marginTop: 5
   },
-  // buttonStyle: {
-  //   borderWidth: 1,
-  //   borderRadius: 2,
-  //   backgroundColor:'#81cdc7',
-  //   borderColor: '#ddd',
-  //   borderBottomWidth: 0,
-  //   shadowColor: '#ddd',
-  //   shadowOffset: { width: 5, height: 2 },
-  //   shadowOpacity: 1,
-  //   shadowRadius: 5,
-  //   elevation: 1,
-  //   marginLeft: 5,
-  //   marginRight: 5,
-  //   marginTop: 10,
-  //   elevation: 1
-  // }
+  menuCarditem:{ 
+    paddingTop: 0, 
+    marginTOP: 0, 
+    paddingBottom: 0
+  },
+  menuCardView:{
+    flex: 1, 
+    flexDirection: 'row', 
+    alignItem: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    borderBottomColor: '#ececec', 
+    borderBottomWidth: 1, 
+    paddingBottom: 10,
+    paddingTop: 10 
+  },
+  menuCardIcon:{
+    height: 40, 
+    width: 40
+  },
+  menuCardTxt:{
+    flex: 1, 
+    flexDirection: 'row', 
+    color: '#606060',
+    paddingLeft: 10
+  },
+  appHdr:{
+    backgroundColor: '#80cec8'
+  },
+  appHdrtitleWarp:{
+    textAlign: 'center', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+
+pname:{ 
+  color: '#5e5e5e', 
+  fontSize: 20, 
+},
+pemail:{ 
+  color: '#969696', 
+  fontSize: 14, 
+},
+pphone:{ 
+  color: '#4b4b4b', 
+  fontSize: 14, 
+},
+pcard:{
+  paddingBottom: 0
+},
+pBtmTxt:{ 
+  flex: 1, 
+  borderTopColor: '#ececec', 
+  borderTopWidth: 1, 
+  paddingTop: 15, 
+  paddingBottom: 5, 
+},
+profileImage:{ 
+  height: 70, 
+  width: 70, 
+  borderRadius: 70, 
+  marginRight: 15 
+},
+pBtmTxt_Txt:{ 
+  textAlign: 'right',
+  color: '#060606' 
+},
+flx_View:{ 
+  flex: 1, 
+  flexDirection: 'row' 
+},
+artNt:{ 
+  width: 20, 
+  textAlign: 'right' 
+},
+artNtTxt:{
+  color: '#ff0026'
+},
+arw_lft:{ 
+  width: 20, 
+  textAlign: 'right' 
+},
+arw_lft_img: { 
+  height: 20, 
+  width: 10 
+},
+version:{
+  textAlign: 'center',
+  fontSize: 10,
+  paddingTop: 15,
+  paddingBottom: 15
+}
 };
