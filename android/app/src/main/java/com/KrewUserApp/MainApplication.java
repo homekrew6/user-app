794b45af4d3760d882c7672962e98b7c4581f542
Anup Chakraborty <ravisingh@natitsolved.com>
2018-02-08 21:05:08 +0530

    latest code

--- android/app/src/main/java/com/KrewUserApp/MainApplication.java
+++ android/app/src/main/java/com/KrewUserApp/MainApplication.java
@@ -3,7 +3,7 @@ package com.KrewUserApp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.reactnative.androidsdk.FBSDKPackage;
+//import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage(),
+            //new FBSDKPackage(),
             new RNI18nPackage(),
             new VectorIconsPackage()
       );
