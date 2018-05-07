7a0eb7a91604f3c1daef38e6ef41793a65586684
krishnendubhattacharya <nits.krishnendu@gmail.com>
2018-05-07 11:57:56 +0530

    eslint

--- js/config.js
+++ js/config.js
@@ -1,12 +1,9 @@
 const config = {
-  base_api : 'http://192.168.1.37:3000/api/',
+  // base_api : 'http://192.168.1.37:3000/api/',
   // base_api : 'http://192.168.1.118:3000/api/',
-  //  base_api: 'http://111.93.169.90:3000/api/',
+   base_api: 'http://111.93.169.90:3000/api/',
   krew_vertical_id: 1,
   s3: {
-
-
-    
     keyPrefix: '',
     bucket: 'homekrewbooking',
     region: 'eu-central-1',
